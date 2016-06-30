f7bea099d14a8eb2f1317e641283fa686383f36b
Syed Afnan <syedafnanfreelancer@gmail.com>
2016-06-30 14:45:57 +0600

    Create options.php

---
+++ options.php
@@ -0,0 +1,95 @@
+$options[] = array(
+  'name'        => 'general',
+  'title'       => 'General',
+  'icon'        => 'fa-globe fa',
+  'fields'      => array()
+  );
+$options[] = array(
+  'name'        => 'homepage',
+  'title'       => 'Home',
+  'icon'        => 'fa fa-home',
+  'fields'      => array()
+  );
+$options[] = array(
+  'name'        => 'header',
+  'title'       => 'Header',
+  'icon'        => 'fa fa-header',
+  'fields'      => array()
+  );
+$options[] = array(
+  'name'        => 'single',
+  'title'       => 'Single',
+  'icon'        => 'fa fa-pagelines',
+  'fields'      => array()
+  );
+$options[] = array(
+  'name'        => 'footer',
+  'title'       => 'Footer',
+  'icon'        => '',
+  'fields'      => array()
+  );
+$options[] = array(
+  'name'        => 'style',
+  'title'       => 'Style',
+  'icon'        => 'fa fa-leaf',
+  'fields'      => array()
+  );
+$options[] = array(
+  'name'        => 'social',
+  'title'       => 'Social',
+  'icon'        => 'fa-medium fa',
+  'fields'      => array()
+  );
+$options[] = array(
+  'name'        => 'advertisement',
+  'title'       => 'Advertisement',
+  'icon'        => 'fa-bullhorn fa',
+  'fields'      => array()
+  );
+$options[] = array(
+  'name'        => 'seo',
+  'title'       => 'SEO',
+  'icon'        => 'fa fa-bolt',
+  'fields'      => array()
+  );
+$options[] = array(
+  'name'        => 'customcss',
+  'title'       => 'Custom Css',
+  'icon'        => 'fa fa-css3',
+  'fields'      => array()
+  );
+$options[] = array(
+  'name'        => 'customjs',
+  'title'       => 'Custom JS',
+  'icon'        => 'fa fa-code',
+  'fields'      => array()
+  );
+$options[] = array(
+  'name'        => 'typography',
+  'title'       => 'Typography',
+  'icon'        => 'fa fa-font',
+  'fields'      => array()
+  );
+$options[] = array(
+  'name'        => 'support',
+  'title'       => 'Support',
+  'icon'        => 'fa-support fa',
+  'fields'      => array()
+  );
+$options[]   = array(
+  'name'     => 'license_section',
+  'title'    => 'License',
+  'icon'     => 'fa fa-info-circle',
+  'fields'   => array(
+
+    array(
+      'type'    => 'heading',
+      'content' => '100% GPL License, Yes it is free!'
+    ),
+    array(
+      'type'    => 'content',
+      'content' => 'Codestar Framework is <strong>free</strong> to use both personal and commercial. If you used commercial, <strong>please credit</strong>. Read more about <a href="http://www.gnu.org/licenses/gpl-2.0.txt" target="_blank">GNU License</a>',
+    ),
+
+  )
+);
